ed11c42466a1353a9b591c537212a851b85250fa
MCal88 <caliendomrc@gmail.com>
2024-10-03 19:56:58 -0500

    added gpt-2 support

--- .venv/main.py
+++ .venv/main.py
@@ -1,15 +1,23 @@
-import voice
-import nlp
+from xml.etree.ElementInclude import include
+
+from voice import response, listening, get_command
+#from nlp import tagging
+from gpt import ai
 
 # Wake word is currently changed here
 wake_word = 'computer'
 
 def main():
     while True:
-        if (voice.listening(wake_word) == True):
-            command = voice.get_command()
-            voice.response(command)
-            nlp.tagging(command)
+        if (listening(wake_word) == True):
+            command = get_command()
+    #        response(command)
+            ai(command)
+    #        nlp.tagging(command)
+
+    #For debugging and avoiding the wake word requirement
+    #    command = get_command()
+    #    ai(command)
 
 
 if __name__ == "__main__":

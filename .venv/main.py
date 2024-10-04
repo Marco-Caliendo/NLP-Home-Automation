1ccd22f910ceb6c0f6d0cb57690324df582d8037
MCal88 <caliendomrc@gmail.com>
2024-10-03 20:02:10 -0500

    added debug function to main

--- .venv/main.py
+++ .venv/main.py
@@ -7,18 +7,22 @@ from gpt import ai
 # Wake word is currently changed here
 wake_word = 'computer'
 
+
+# For debugging and avoiding the wake word requirement
+def debug():
+    while True:
+        command = get_command()
+        ai(command)
+
 def main():
     while True:
         if (listening(wake_word) == True):
             command = get_command()
-    #        response(command)
             ai(command)
-    #        nlp.tagging(command)
-
-    #For debugging and avoiding the wake word requirement
-    #    command = get_command()
-    #    ai(command)
+            #response(command)
+            #nlp.tagging(command)
 
 
 if __name__ == "__main__":
     main()
+    #debug()

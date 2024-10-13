46cfea44b7d6b182d3d9520604b7aa948ac59244
MCal88 <caliendomrc@gmail.com>
2024-10-12 21:03:06 -0500

    Changed some comments and moved some files

--- .venv/main.py
+++ .venv/main.py
@@ -1,7 +1,6 @@
 from xml.etree.ElementInclude import include
 
 from voice import response, listening, get_command
-#from nlp import tagging
 from gpt import ai
 from command_classification import comclass
 
@@ -9,35 +8,43 @@ from command_classification import comclass
 wake_word = 'computer'
 
 
-# For debugging and avoiding speech to text
+# For testing and avoiding speech to text
 def text_only():
     while True:
         command = input("User Input: ")
+        # Is the input an automation command or a conversational input
         command_class = comclass(command)[0]
+        # If conversation, send to AI chat module
         if command_class == "CONVERSATION":
             ai(command)
+        # If automation, execute automation command
         elif command_class == "AUTOMATION":
             print("Executing command : " + command)
+        # Else return error
         else:
             print("Error")
 
+        # Exit command for testing
         if command == "exit":
             return 0
 
 
+# Main function that calls the speach to text module
 def main():
     while True:
         if (listening(wake_word) == True):
             command = get_command()
+            # Is the input an automation command or a conversational input
             command_class = comclass(command)[0]
+            # If conversation, send to AI chat module
             if command_class == "CONVERSATION":
                 ai(command)
+            # If automation, execute automation command
             elif command_class == "AUTOMATION":
                 print("Executing command : " + command)
+            # Else return error
             else:
                 print("Error")
-            #response(command)
-            #nlp.tagging(command)
 
 
 
